d565cfc917e019782d1baca10141911df7d1a950
Brayo254 <b.itumo18@gmail.com>
2023-03-16 16:01:27 +0300

    err3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 *main-main starting point. 
-*Return:0 (success)
+*Return:0 (success).
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}		
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-  *main - beginning 
-  *Return:0
+*main - beginning point 
+*Return:always 0(success)
 */
 int main(void)
-{
-	printf:("with proper grammar, but the outcome is a piece of art,\n");
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
