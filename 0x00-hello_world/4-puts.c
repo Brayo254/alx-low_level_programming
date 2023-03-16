e30a5b5bf50c6efc0ebed077ae9d0896d5db5e1c
Brayo254 <b.itumo18@gmail.com>
2023-03-16 15:19:01 +0300

    bug 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
-*How to use the function puts.
-* main - this is a starting point of a program.
-*Return: Endpoint of a fuction that is to be or has been executed.
-*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }		
