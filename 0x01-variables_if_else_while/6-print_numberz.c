7009f7e34efbf76ec00983b5f3834f5d3f17328d
Brayo254 <b.itumo18@gmail.com>
2023-03-17 13:30:42 +0300

    more numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints out digits from 0 to 9.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 72; i < 82; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
