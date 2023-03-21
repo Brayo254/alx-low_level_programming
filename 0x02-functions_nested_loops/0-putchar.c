65a188bfc14742e65c2183cdebb7b8957ee75f4c
Brayo254 <b.itumo18@gmail.com>
2023-03-21 12:14:14 +0300

    char fixing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *main _putchar on a new line one by one.
+ *main - printing putchar on a new line one by one.
  *every character is going to be displayed one after the other.
  * Return: On success 0.
  */
