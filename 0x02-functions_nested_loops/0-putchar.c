5f0b85de55823a6341bcb9a085f00209c4c621ec
Brayo254 <b.itumo18@gmail.com>
2023-03-21 12:10:50 +0300

    char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main _putchar on a new line one by one.
+ *every character is going to be displayed one after the other.
+ * Return: On success 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
