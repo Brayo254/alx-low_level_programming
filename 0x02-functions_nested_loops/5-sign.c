6931f52d8820ed81c3ed305ea66ca541853c8a74
Brayo254 <b.itumo18@gmail.com>
2023-03-21 16:09:12 +0300

    sign7

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_sign - prints the signs
  * @n: checking of arguement
@@ -10,16 +11,12 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
+	_putchar('+');
+	return (1);
 	}
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar('-');
+	_putchar('+');
+	return (0);
 	}
-		return (-1);
 }
-
